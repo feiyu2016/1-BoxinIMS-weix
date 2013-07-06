3725ea5a58f92c2907e4859eac98d0ac6cd91e34
jakemanse@163.com <Jakemanse@Jake>
2013-07-06 08:54:40 +0800

    添加支付宝接口

---
+++ src/main/java/com/alipay/client/base/PartnerConfig.java
@@ -0,0 +1,11 @@
+package com.alipay.client.base;
+
+public class PartnerConfig {
+    //合作商户ID。用签约支付宝账号登录ms.alipay.com后，在账户信息页面获取。
+    public static final String PARTNER = "2088011183300795";
+    // 商户收款的支付宝账号
+    public static final String SELLER = "hdzhixun@163.com";
+    // 商户（MD5）KEY
+    public static final String KEY="ctzorml8rx0ejc1gh6dchbaa4dgufqos";
+	
+}
